59febeeb163262cd92291bbd7c5a596d31b070b0
WiktorSusfal <44230240+WiktorSusfal@users.noreply.github.com>
2021-06-06 15:21:59 +0200

    Add files via upload

--- UR_Connections.py
+++ UR_Connections.py
@@ -1,5 +1,9 @@
 import socket
 
+<<<<<<< HEAD
+
+=======
+>>>>>>> e455e9b6e43f8e6ae84d563feb398e344a89cb48
 class UR_connection:
     UR_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     UR_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -8,11 +12,26 @@ class UR_connection:
     port = 0
     refresh_interval = 0.0
     data_buffer = []
+<<<<<<< HEAD
+    robot_name = ''
+    terminate = False
+
+    def __init__(self, ip, port, ref_freq=-1, robot_name='emptyName', terminate=False):
+
+        self.ip = ip
+        self.port = port
+        self.robot_name = robot_name
+        # Used for terminate threads in application GUI. Based on this property, the data retrieval is broken and the
+        # connection is terminated
+
+        self.terminate = terminate
+=======
 
     def __init__(self, ip, port, ref_freq=-1):
 
         self.ip = ip
         self.port = port
+>>>>>>> e455e9b6e43f8e6ae84d563feb398e344a89cb48
 
         if ref_freq != -1:
             self.refresh_interval = 1.0 / ref_freq
@@ -35,16 +54,38 @@ class UR_connection:
         elif ref_freq == -1:
             self.refresh_interval = 1.0
 
+<<<<<<< HEAD
+
+    def connect_to_UR(self):
+
+        if self.interface_name != 'Not known interface, probably mistake':
+            print(f'{self.robot_name} - creating connection with server')
+            self.UR_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.UR_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+=======
     def connect_to_UR(self):
 
         if self.interface_name not in ['Not known interface, probably mistake']:
+>>>>>>> e455e9b6e43f8e6ae84d563feb398e344a89cb48
             self.UR_server.connect((self.ip, self.port))
         else:
             print('\nCannot connect to not known interface\n')
 
     def disconnect_from_UR(self):
+<<<<<<< HEAD
+        print(f'{self.robot_name} - closing connection with server')
+        self.UR_server.close()
+
+    def return_data(self):
+
+        self.data_buffer = self.UR_server.recv(4096)
+        return self.data_buffer
+
+
+=======
         self.UR_server.close()
 
     def return_data(self):
         self.data_buffer = self.UR_server.recv(4096)
         return self.data_buffer
+>>>>>>> e455e9b6e43f8e6ae84d563feb398e344a89cb48
